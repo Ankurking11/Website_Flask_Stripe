b563719d8fc0fc4dfed10690e99f5e87fc273234
Ankurking11 <ankurnoida11@gmail.com>
2024-06-06 16:46:39 +0530

    fully functional

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 import razorpay
-# from models import db, User, Product, Transaction
-# from config import Config
-#import razorpay
+import logging
+import razorpay
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -50,7 +49,7 @@ class Transaction(db.Model):
 @app.route('/')
 def index():
     #products = Product.query.all()#remove this
-    return render_template('register.html')#change this 
+    return render_template('login.html')#change this 
 
 
 #register
@@ -106,8 +105,26 @@ def buy(product_id):
 
 
 #success
-@app.route('/success', methods=['GET','POST'])
-def success():
-    return render_template("success.html")
+@app.route('/success/<order_id>/<product_id>', methods=['GET','POST'])
+def success(order_id,product_id):
+    order_id = order_id
+    prod_id = product_id
+    user_id=session['user_id']
+    client = razorpay.Client(auth = ("rzp_test_I9GZQqSmWyMm0h" , "Ft34wtwLr0olMmEaK0GV5vJb"))
+    order_details = client.order.payments(order_id)
+    transaction_id = order_details['items'][0]['id']
+    amount = order_details['items'][0]['amount']/100
+    status = order_details['items'][0]['status']
+
+    transaction = Transaction(product_id=prod_id, user_id=user_id, 
+                              transaction_id=transaction_id, 
+                              amount=amount)
+    db.session.add(transaction)
+    db.session.commit()
+
+    return render_template("success.html",transaction_id=transaction_id, 
+                           amount=amount,status=status)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
